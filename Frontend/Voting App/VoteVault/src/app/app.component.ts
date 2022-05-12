89c73b8085a528c542e81d8ec09c448cb013b9fd
fffrikkie <u20532581@tuks.co.za>
2022-05-12 10:04:40 +0200

    Adds routing through tabs on left hand side

--- Frontend/Voting App/VoteVault/src/app/app.component.ts
+++ Frontend/Voting App/VoteVault/src/app/app.component.ts
@@ -6,13 +6,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Register Voter', url: '/register', icon: 'mail' },
+    { title: 'Generate Ballots', url: '/generate-ballot', icon: 'paper-plane' },
+    { title: 'Ballots', url: '/ballot', icon: 'heart' },
+    { title: 'Login', url: '/login', icon: 'heart' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
