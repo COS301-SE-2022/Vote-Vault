441564590b83a5b7e30290d2c61ebc1f25809561
fffrikkie <u20532581@tuks.co.za>
2022-05-12 11:25:21 +0200

    Commited

--- Frontend/Voting App/VoteVault/src/app/app-routing.module.ts
+++ Frontend/Voting App/VoteVault/src/app/app-routing.module.ts
@@ -18,7 +18,8 @@ const routes: Routes = [
   {
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
-  },  {
+  },
+  {
     path: 'ballot',
     loadChildren: () => import('./ballot/ballot.module').then( m => m.BallotPageModule)
   },
