80a834983c0b571452b52041b86c61bbd655fe55
mvantubbergh <mvantubbergh@gmail.com>
2022-06-06 02:55:12 +0200

    :ambulance: fixed spec file

--- Frontend/Website/Vote-Vault-Website/src/app/app.component.spec.ts
+++ Frontend/Website/Vote-Vault-Website/src/app/app.component.spec.ts
@@ -1,61 +1,112 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { Component, OnInit } from '@angular/core';
 
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+@Component({
+  selector: 'app-analytics-page',
+  templateUrl: './analytics-page.component.html',
+  styleUrls: ['./analytics-page.component.css']
+})
+export class AnalyticsPageComponent implements OnInit {
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  votes : any[]
 
-  it(`should have as title 'Vote-Vault-Website'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Vote-Vault-Website');
-  });
+  constructor() { 
+    this.votes = [{"electionID" : 2, "candidateID" : ["3", "5", "5"], "gender" : "male", "age" : 25, "location" : "Cape Town"}]
+  }
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Vote-Vault-Website app is running!');
-  });
-});
+  ngOnInit(): void {
+    
+  }
 
-div0:boolean=true;
-div1:boolean=false;
-div2:boolean=false;
-div3:boolean=false;
+  type = 'bar';
+  data = {
+    labels: ["Male", "Female"],
+    datasets: [
+      {
+        backgroundColor: ['blue','red'],
+        data: [59, 59]
+      }
+    ]
+  };
+  options = {
+    legend:{
+      display: false
+    },
+    title:{
+      display: true,
+      text:"Gender Distribution"
+    },
+    responsive: true,
+    maintainAspectRatio: false
+  };
 
-showGenderInfo(){
-  this.div0=false;
-  this.div1=true;
-  this.div2=false;
-  this.div3=false
-}
+  type1 = 'bar';
+  data1 = {
+    labels: ["Gauteng","Freestate","Kwazulu-Natal","Mpumalanga","Limpopo","Eastern Cape","North West","Northern Cape","Western Cape"],
+    datasets: [
+      {
+        backgroundColor: ['blue','red','green','yellow','purple','grey','brown','teal','cyan'],
+        data: [60, 61, 59, 42, 45, 48, 51, 52, 42]
+      }
+    ]
+  };
+  options1 = {
+    legend:{
+      display: false
+    },
+    title:{
+      display: true,
+      text:"Location Distribution"
+    },
+    responsive: true,
+    maintainAspectRatio: false
+  };
 
-showAgeInfo(){
-  this.div0=false;
-  this.div2=true;
-  this.div1=false;
-  this.div3=false
-}
+  type2 = 'bar';
+  data2 = {
+    labels: ["18-28","29-38","39-48","49-58","59-68","69-78","79-88","89-98","99+"],
+    datasets: [
+      {
+        backgroundColor: ['blue','red','green','yellow','purple','grey','brown','teal','cyan'],
+        data: [60, 61, 59, 42, 55, 48, 43, 52, 5]
+      }
+    ]
+  };
+  options2 = {
+    legend:{
+      display: false
+    },
+    title:{
+      display: true,
+      text:"Age Distribution"
+    },
+    responsive: true,
+    maintainAspectRatio: false
+  };
 
-showProvinceInfo(){
-  this.div0=false;
-  this.div3=true;
-  this.div2=false;
-  this.div1=false
+  div0:boolean=true;
+  div1:boolean=false;
+  div2:boolean=false;
+  div3:boolean=false;
+
+  showGenderInfo(){
+      this.div0=false;
+      this.div1=true;
+      this.div2=false;
+      this.div3=false
+  }
+
+  showAgeInfo(){
+      this.div0=false;
+      this.div2=true;
+      this.div1=false;
+      this.div3=false
+  }
+
+  showProvinceInfo(){
+      this.div0=false;
+      this.div3=true;
+      this.div2=false;
+      this.div1=false
+  }
+
 }
