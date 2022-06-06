5b0fe220f6fb4e3d43d9c38cc38873420ddcd343
Bevan Slabbert <89096655+bevanslabbert@users.noreply.github.com>
2022-06-06 19:58:50 +0200

    :sparkles: Admin Dashboard : added menu

--- Frontend/Voting App/VoteVault/src/app/admin-dashboard/admin-dashboard.page.ts
+++ Frontend/Voting App/VoteVault/src/app/admin-dashboard/admin-dashboard.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminDashboardPage implements OnInit {
 
-  constructor() { }
+  constructor(private menu : MenuController) { }
 
   ngOnInit() {
+  }
+
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+
+  openEnd() {
+    this.menu.open('end');
+  }
+
+  openCustom() {
+    this.menu.enable(true, 'custom');
+    this.menu.open('custom');
   }
 
 }
