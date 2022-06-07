21bd79b336c5fedb158e924bdc22deb4ffaa8520
JG (Hanroe) Geyser <u20450932@tuks.co.za>
2022-06-07 20:58:52 +0200

    Fixed app karma.config

--- Frontend/Voting App/VoteVault/karma.conf.js
+++ Frontend/Voting App/VoteVault/karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['ChromeHeadless','ChromeHeadlessCI'],
+    browsers: ['ChromeHeadless'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
