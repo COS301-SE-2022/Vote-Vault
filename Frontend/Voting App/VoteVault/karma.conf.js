60fb536a15d06c1b8c10fc18c061fe88e67c7c7b
JG (Hanroe) Geyser <u20450932@tuks.co.za>
2022-06-07 19:54:47 +0200

    Trying another fix

--- Frontend/Voting App/VoteVault/karma.conf.js
+++ Frontend/Voting App/VoteVault/karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['firefox','Chrome','ChromeHeadless','ChromeHeadlessCI'],
+    browsers: ['ChromeHeadless','ChromeHeadlessCI'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
