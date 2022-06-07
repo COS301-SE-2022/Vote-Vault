a595a03f3a221ff5236e87fc6e75e2c97a2d5e8e
JG (Hanroe) Geyser <u20450932@tuks.co.za>
2022-06-07 20:41:29 +0200

    Fixed Auto testing

--- Frontend/Website/Vote-Vault-Website/karma.conf.js
+++ Frontend/Website/Vote-Vault-Website/karma.conf.js
@@ -37,9 +37,15 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: [],
-    singleRun: false,
+    autoWatch: false,
+    browsers: ['ChromeHeadless','ChromeHeadlessCI'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu']
+      }
+    },
+    singleRun: true,
     restartOnFileChange: true
   });
 };
