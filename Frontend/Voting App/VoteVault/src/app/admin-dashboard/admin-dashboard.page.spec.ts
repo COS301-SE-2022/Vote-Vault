5306935425e7c9f9fb3d8214630c69bd40e765d7
JG (Hanroe) Geyser <u20450932@tuks.co.za>
2022-06-15 12:59:02 +0200

    More fixes

--- Frontend/Voting App/VoteVault/src/app/admin-dashboard/admin-dashboard.page.spec.ts
+++ Frontend/Voting App/VoteVault/src/app/admin-dashboard/admin-dashboard.page.spec.ts
@@ -13,6 +13,10 @@ describe('AdminDashboardPage', () => {
   let component: AdminDashboardPage;
   let fixture: ComponentFixture<AdminDashboardPage>;
 
+  beforeAll(waitForAsync(() => {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 999999;
+  }));
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AdminDashboardPage ],
@@ -27,11 +31,15 @@ describe('AdminDashboardPage', () => {
     fixture.detectChanges();
   }));
 
+  it('should have custom timeout', () => {
+    console.log(jasmine.DEFAULT_TIMEOUT_INTERVAL); //prints 999999
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  // it('should have an elections class member', () => {
-  //   expect(component.elections).toEqual(jasmine.any(Object));
-  // })
+  it('should have an elections class member', () => {
+    expect(component.elections).toEqual(jasmine.any(Object));
+  });
 });
