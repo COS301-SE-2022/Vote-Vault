e5bacbf59c59e684bdd0e140b0722330b5913ae2
Bevan Slabbert <89096655+bevanslabbert@users.noreply.github.com>
2022-07-21 09:34:22 +0200

    Added vote struct to smart contract

--- Backend/Blockchain/contracts/Election.sol
+++ Backend/Blockchain/contracts/Election.sol
@@ -1,5 +1,11 @@
  pragma solidity^0.5.0;
 
  contract Election{
-    
+    //Store users that are allowed to vote in this election in an array
+    //Once removed, they have voted
+    struct Vote {
+        uint age;
+        bool gender;    //true for Male, false for Female
+        uint votes[3];  //stores indices
+    }
  }
