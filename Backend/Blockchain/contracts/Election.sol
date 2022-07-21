1b2e36dab1c8ecb261cde6a6c3d029cbe83cce22
Bevan Slabbert <89096655+bevanslabbert@users.noreply.github.com>
2022-07-21 09:44:13 +0200

    Update Election.sol

--- Backend/Blockchain/contracts/Election.sol
+++ Backend/Blockchain/contracts/Election.sol
@@ -1,10 +1,16 @@
  pragma solidity^0.5.0;
 
  contract Election {
+    
     uint [3][] public voteCount;
     uint public electionID;
-<<<<<<< HEAD
     uint public startDate;
     uint public endDate;
     byte32 [] public voters;
+
+    struct Vote {
+        uint age;
+        bool gender;    //true for Male, false for Female
+        uint votes[3];  //stores indices
+    }
 }
