834b31ff40e059f57beefc3ad8578f32b2ec592f
Bevan Slabbert <89096655+bevanslabbert@users.noreply.github.com>
2022-07-22 14:29:02 +0200

    Configuration for Blockchain
    Downgraded node version to v13.
    Added abi.js file
    Added .env file

--- Backend/Blockchain/contracts/Election.sol
+++ Backend/Blockchain/contracts/Election.sol
@@ -6,40 +6,40 @@ pragma solidity^0.5.0;
     uint public electionID;
     uint public startDate;
     uint public endDate;
-    byte32 [] public voters;
+    bytes32 [] public voters;
 
     struct Vote {
         uint age;
         bool gender;    //true for Male, false for Female
-        uint votes[3];  //stores indices
+        uint [3] votes;  //stores indices
     }
 
     function addVote(uint ballot, uint candidate) external {
         voteCount[ballot][candidate] += 1;
     }
 
-    function countVotes() public view {
-        uint count = 0;
+    // function countVotes() public view {
+    //     uint count = 0;
 
-        for(uint i=0; i>0; i++) {
-            for( uint j=0; j<=i; j++) {
-                if (voteCount[i][j] > 0) {
-                    count++;
-                }
-                else {
-                    break;
-                }
-            }
-        }
+    //     for(uint i=0; i>0; i++) {
+    //         for( uint j=0; j<=i; j++) {
+    //             if (voteCount[i][j] > 0) {
+    //                 count++;
+    //             }
+    //             else {
+    //                 break;
+    //             }
+    //         }
+    //     }
 
-        returns (uint count)
+    //     returns (uint count)
 
-    }
+    // }
 
-    function votePeriod() public view {
-        uint days = (endDate - startDate) / 60 / 60 / 24;
-        returns (uint days)
-    }
+    // function votePeriod() public view {
+    //     uint days = (endDate - startDate) / 60 / 60 / 24;
+    //     returns (uint days)
+    // }
 
 }
 
--- Backend/Blockchain/truffle-config.js
+++ Backend/Blockchain/truffle-config.js
@@ -1,15 +1,18 @@
-module_exports = {
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const { INFURA_API_URL, MNEMONIC } = process.env;
+
+module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" //Any network ID
-    }
-  },
-  solc: {
-    optimizer: {
-      enabled: true,
-      runs: 200
+      port: 8545,
+      network_id: "*"
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(MNEMONIC, INFURA_API_URL),
+      network_id: 3,
+      gas: 5500000,
     }
   }
-}
+};
