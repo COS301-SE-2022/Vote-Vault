3691bd4d00a6563f3c2cd51ebd27ef91b610e21b
JG (Hanroe) Geyser <u20450932@tuks.co.za>
2022-07-23 13:18:48 +0200

    Barcode scanner test 1

--- Frontend/Voting App/VoteVault/src/app/voter-registration/voter-registration.page.ts
+++ Frontend/Voting App/VoteVault/src/app/voter-registration/voter-registration.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-voter-registration',
@@ -7,55 +8,79 @@ import { Router } from '@angular/router';
   styleUrls: ['./voter-registration.page.scss'],
 })
 export class VoterRegistrationPage implements OnInit {
-  name: string
-  surname: string
-  idNum: string
-  voterNames: any[]
-  voterSurnames: any[]
-  voterIDs: any[]
+  name: string;
+  surname: string;
+  idNum: string;
+  voterNames: any[];
+  voterSurnames: any[];
+  voterIDs: any[];
+  scannedData: any;
+  encodedData: '';
+  encodeData: any;
+  inputData: any;
 
-  constructor(private router : Router) { }
+  constructor(private router: Router, private barcodeScanner: BarcodeScanner) { }
 
   ngOnInit() {
-    this.voterIDs = []
-    this.voterNames = []
-    this.voterSurnames = []
+    this.voterIDs = [];
+    this.voterNames = [];
+    this.voterSurnames = [];
   }
 
-  registerVoter() : void {
-    const nVoter = {"name" : this.name, "surname" : this.surname, "id" : this.idNum, "isRegistered" : false}
-    
+  registerVoter(): void {
+    const nVoter = {name : this.name, surname : this.surname, id : this.idNum, isRegistered : false};
+
     if (nVoter.name === undefined || nVoter.surname === undefined || nVoter.id === undefined) {
-      alert("Please make sure all fields are filled in.")
-      return
+      alert('Please make sure all fields are filled in.');
+      return;
     }
 
-    if (nVoter.id.length != 13) {
-      alert("Please enter an ID number with a length of 13.")
-      return
+    if (nVoter.id.length !== 13) {
+      alert('Please enter an ID number with a length of 13.');
+      return;
     }
 
     if (!/^[0-9]+$/.test(nVoter.id)) {
-      alert("Please make sure the ID only exists of digits.")
-      return
+      alert('Please make sure the ID only exists of digits.');
+      return;
     }
 
-    const first = this.voterIDs.find((obj) => {
-      return obj === nVoter.id;
-    });
+    const first = this.voterIDs.find((obj) => obj === nVoter.id);
     if (first != null) {
-      alert("The entered ID is already registered with a voter.")
-      return
+      alert('The entered ID is already registered with a voter.');
+      return;
     }
-    this.voterNames.push(nVoter.name)
-    this.voterSurnames.push(nVoter.surname)
-    this.voterIDs.push(nVoter.id)
-    this.name = ""
-    this.surname = ""
-    this.idNum = ""
+    this.voterNames.push(nVoter.name);
+    this.voterSurnames.push(nVoter.surname);
+    this.voterIDs.push(nVoter.id);
+    this.name = '';
+    this.surname = '';
+    this.idNum = '';
   }
 
   openCustom() {
-    this.router.navigate(['admin-dashboard'])
+    this.router.navigate(['admin-dashboard']);
   }
+
+  scanBarcode() {
+    const options: BarcodeScannerOptions = {
+      preferFrontCamera: false,
+      showFlipCameraButton: true,
+      showTorchButton: true,
+      torchOn: false,
+      prompt: 'Place a barcode inside the scan area',
+      resultDisplayDuration: 500,
+      formats: 'EAN_13,EAN_8,QR_CODE,PDF_417 ',
+      orientation: 'portrait',
+    };
+
+    this.barcodeScanner.scan(options).then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+      this.scannedData = barcodeData;
+
+    }).catch(err => {
+      console.log('Error', err);
+    });
+  }
+
 }
