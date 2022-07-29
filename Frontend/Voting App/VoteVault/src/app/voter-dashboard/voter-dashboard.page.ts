83e5ef69d1e383719e7981a242fd46632596e3a0
Bevan Slabbert <89096655+bevanslabbert@users.noreply.github.com>
2022-07-29 09:33:29 +0200

    Frontend touch ups

--- Frontend/Voting App/VoteVault/src/app/voter-dashboard/voter-dashboard.page.ts
+++ Frontend/Voting App/VoteVault/src/app/voter-dashboard/voter-dashboard.page.ts
@@ -57,4 +57,8 @@ export class VoterDashboardPage implements OnInit {
     const { role, data } = await actionSheet.onDidDismiss();
     console.log('onDidDismiss resolved with role and data', role, data);
   }
+
+  signOut() {
+    this.router.navigate(['login'])
+  }
 }
