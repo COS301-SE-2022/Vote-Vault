7bd896d441a72ee2e1ec04d0ebe4eaabd479450d
Bevan Slabbert <89096655+bevanslabbert@users.noreply.github.com>
2022-08-01 14:10:23 +0200

    :lipstick: (UI) : Updated voter dashboard and ballot front end

--- Frontend/Voting App/VoteVault/src/app/voter-dashboard/voter-dashboard.module.ts
+++ Frontend/Voting App/VoteVault/src/app/voter-dashboard/voter-dashboard.module.ts
@@ -7,6 +7,8 @@ import { IonicModule } from '@ionic/angular';
 import { VoterDashboardPageRoutingModule } from './voter-dashboard-routing.module';
 
 import { VoterDashboardPage } from './voter-dashboard.page';
+import { ElectionsHeaderComponent } from '../components/elections-header/elections-header.component';
+import { CircleTopComponent } from '../components/circle-top/circle-top.component';
 
 @NgModule({
   imports: [
@@ -15,6 +17,6 @@ import { VoterDashboardPage } from './voter-dashboard.page';
     IonicModule,
     VoterDashboardPageRoutingModule
   ],
-  declarations: [VoterDashboardPage]
+  declarations: [VoterDashboardPage, ElectionsHeaderComponent, CircleTopComponent]
 })
 export class VoterDashboardPageModule {}
