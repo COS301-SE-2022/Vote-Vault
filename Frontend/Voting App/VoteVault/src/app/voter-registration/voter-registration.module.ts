d35fd0e1a226d953e1c6ef0c463cec1f32a2152d
Bevan Slabbert <89096655+bevanslabbert@users.noreply.github.com>
2022-08-01 14:41:22 +0200

    UI updates

--- Frontend/Voting App/VoteVault/src/app/voter-registration/voter-registration.module.ts
+++ Frontend/Voting App/VoteVault/src/app/voter-registration/voter-registration.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { VoterRegistrationPageRoutingModule } from './voter-registration-routing.module';
 
 import { VoterRegistrationPage } from './voter-registration.page';
+import { CircleTopComponent } from '../components/circle-top/circle-top.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { VoterRegistrationPage } from './voter-registration.page';
     IonicModule,
     VoterRegistrationPageRoutingModule
   ],
-  declarations: [VoterRegistrationPage]
+  declarations: [VoterRegistrationPage, CircleTopComponent]
 })
 export class VoterRegistrationPageModule {}
