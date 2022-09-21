9e4d9448312ea539f11b820fb9a54b01845996da
JG (Hanroe) Geyser <u20450932@tuks.co.za>
2022-09-21 10:02:56 +0200

    Keeps graphs and headings hidden until an election is selected

--- Frontend/Website/Vote-Vault-Website/src/app/results-page/results-page.component.ts
+++ Frontend/Website/Vote-Vault-Website/src/app/results-page/results-page.component.ts
@@ -44,7 +44,6 @@ export class ResultsPageComponent implements OnInit {
   electionName : any;
 
   constructor(private dataService : DataService) {
-    this.ballotName1 = "some stuff";
     this.names1 = [];
     this.names2 = [];
     this.names3 = [];
@@ -57,101 +56,105 @@ export class ResultsPageComponent implements OnInit {
   async ngOnInit() {
 
     this.elections = await this.dataService.fetchElections();
-    this.type = 'bar';
-    this.data = {
-      labels: this.results[0].nameslistballot1,
-      datasets: [
-        {
-          backgroundColor: ['#a69cac ','#474973 ','#161b33 ','#0d0c1d','#f1dac4 ','#033f63 ','#323031'],
-          data: this.results[0].bal1
-        }
-      ]
-    };
-    this.options = {
-      legend:{
-        display: false
-      },
-      title:{
-        display: true,
-        text:"Election Results"
-      },
-      scales : {
-        yAxes: [{
-          ticks: {
-              steps : 10,
-              stepValue : 10,
-              max : 100,
-              min: 0
-            }
-        }]
-      },
-      responsive: true,
-      maintainAspectRatio: false
-    };
 
-    this.type1 = 'bar';
-    this.data1 = {
-      labels: this.results[0].nameslistballot2,
-      datasets: [
-        {
-          backgroundColor: ['#353535','#3c6e71 ','#b5fff8','#d9d9d9 ','#284b63','#4f6d7a ','#c0d6df '],
-          data: this.results[0].bal2
-        }
-      ]
-    };
-    this.options1 = {
-      legend:{
-        display: false
-      },
-      title:{
-        display: true,
-        text:"Election Results"
-      },
-      scales : {
-        yAxes: [{
-          ticks: {
-              steps : 10,
-              stepValue : 10,
-              max : 100,
-              min: 0
-            }
-        }]
-      },
-      responsive: true,
-      maintainAspectRatio: false
-    };
+    document.getElementById("info1").hidden = true;
+    document.getElementById("info3").hidden = true;
+    document.getElementById("info5").hidden = true;
+    // this.type = 'bar';
+    // this.data = {
+    //   labels: this.results[0].nameslistballot1,
+    //   datasets: [
+    //     {
+    //       backgroundColor: ['#a69cac ','#474973 ','#161b33 ','#0d0c1d','#f1dac4 ','#033f63 ','#323031'],
+    //       data: this.results[0].bal1
+    //     }
+    //   ]
+    // };
+    // this.options = {
+    //   legend:{
+    //     display: false
+    //   },
+    //   title:{
+    //     display: true,
+    //     text:"Election Results"
+    //   },
+    //   scales : {
+    //     yAxes: [{
+    //       ticks: {
+    //           steps : 10,
+    //           stepValue : 10,
+    //           max : 100,
+    //           min: 0
+    //         }
+    //     }]
+    //   },
+    //   responsive: true,
+    //   maintainAspectRatio: false
+    // };
 
-    this.type2 = 'bar';
-    this.data2 = {
-      labels: this.results[0].nameslistballot3,
-      datasets: [
-        {
-          backgroundColor: ['#fdc500 ','#fedc97','#ffc857  ','#fcbf49 ','#00509d ','#00296b ','#003f88 '],
-          data: this.results[0].bal3
-        }
-      ]
-    };
-    this.options2 = {
-      legend:{
-        display: false
-      },
-      title:{
-        display: true,
-        text:"Election Results"
-      },
-      scales : {
-        yAxes: [{
-          ticks: {
-              steps : 10,
-              stepValue : 10,
-              max : 100,
-              min: 0
-            }
-        }]
-      },
-      responsive: true,
-      maintainAspectRatio: false
-    }
+    // this.type1 = 'bar';
+    // this.data1 = {
+    //   labels: this.results[0].nameslistballot2,
+    //   datasets: [
+    //     {
+    //       backgroundColor: ['#353535','#3c6e71 ','#b5fff8','#d9d9d9 ','#284b63','#4f6d7a ','#c0d6df '],
+    //       data: this.results[0].bal2
+    //     }
+    //   ]
+    // };
+    // this.options1 = {
+    //   legend:{
+    //     display: false
+    //   },
+    //   title:{
+    //     display: true,
+    //     text:"Election Results"
+    //   },
+    //   scales : {
+    //     yAxes: [{
+    //       ticks: {
+    //           steps : 10,
+    //           stepValue : 10,
+    //           max : 100,
+    //           min: 0
+    //         }
+    //     }]
+    //   },
+    //   responsive: true,
+    //   maintainAspectRatio: false
+    // };
+
+    // this.type2 = 'bar';
+    // this.data2 = {
+    //   labels: this.results[0].nameslistballot3,
+    //   datasets: [
+    //     {
+    //       backgroundColor: ['#fdc500 ','#fedc97','#ffc857  ','#fcbf49 ','#00509d ','#00296b ','#003f88 '],
+    //       data: this.results[0].bal3
+    //     }
+    //   ]
+    // };
+    // this.options2 = {
+    //   legend:{
+    //     display: false
+    //   },
+    //   title:{
+    //     display: true,
+    //     text:"Election Results"
+    //   },
+    //   scales : {
+    //     yAxes: [{
+    //       ticks: {
+    //           steps : 10,
+    //           stepValue : 10,
+    //           max : 100,
+    //           min: 0
+    //         }
+    //     }]
+    //   },
+    //   responsive: true,
+    //   maintainAspectRatio: false
+    // }
   };
 
   async selectElection(e : any) : Promise<void> {
@@ -191,6 +194,9 @@ export class ResultsPageComponent implements OnInit {
     this.results = [{nameslistballot1: this.names1, nameslistballot2: this.names2, nameslistballot3: this.names3, bal1: this.ballot1, bal2: this.ballot2, bal3: this.ballot3}];
 
     console.log(this.ballot1);
+    document.getElementById("info1").hidden = false;
+    document.getElementById("info3").hidden = false;
+    document.getElementById("info5").hidden = false;
 
     this.type = 'bar';
     this.data = {
