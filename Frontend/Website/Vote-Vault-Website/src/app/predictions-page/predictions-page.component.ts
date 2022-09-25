344b4451c6852c64bb299feaed40e22c65b49354
Dhashin Reddy <u18169253@tuks.co.za>
2022-09-25 16:15:53 +0200

    predictions page ts file created

---
+++ Frontend/Website/Vote-Vault-Website/src/app/predictions-page/predictions-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-howto-page',
+  templateUrl: './predictions-page.component.html',
+  styleUrls: ['./predictions-page.component.css']
+})
+export class HowtoPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
