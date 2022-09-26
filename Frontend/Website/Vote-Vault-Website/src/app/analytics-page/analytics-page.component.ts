f1630dbc142e462b0ae7d56774531690b7fc5b06
mvantubbergh <mvantubbergh@gmail.com>
2022-09-27 00:05:55 +0200

    added data for the predictions chart

--- Frontend/Website/Vote-Vault-Website/src/app/analytics-page/analytics-page.component.ts
+++ Frontend/Website/Vote-Vault-Website/src/app/analytics-page/analytics-page.component.ts
@@ -20,6 +20,9 @@ export class AnalyticsPageComponent implements OnInit {
   data2;
   type2;
   options2;
+  data3;
+  type3;
+  options3;
   selectedElection : any
 
   constructor(private dataService : DataService) {
@@ -188,6 +191,39 @@ export class AnalyticsPageComponent implements OnInit {
   async showPredictionsInfo() {
     await this.dataService.fetchAllElections();
     await this.dataService.calculateProbabilities();
+
+
+    this.type3 = 'bar';
+    this.data3 = {
+      labels: ["ANC","DA","EFF","VFP"],
+      datasets: [
+        {
+          backgroundColor: ['#353535','#3c6e71','#b5fff8','#d9d9d9'],
+          data: [this.dataService.predictionsArray[0], this.dataService.predictionsArray[1], this.dataService.predictionsArray[2], this.dataService.predictionsArray[3]]
+        }
+      ]
+    };
+    this.options3= {
+      legend:{
+        display: false
+      },
+      title:{
+        display: true,
+        text:"Election Prediction"
+      },
+      scales : {
+        yAxes: [{
+           ticks: {
+              steps : 10,
+              stepValue : 10,
+              max : 100,
+              min: 0
+            }
+        }]
+      },
+      responsive: true,
+      maintainAspectRatio: false
+    };
   }
 
   showProvinceInfo(){
