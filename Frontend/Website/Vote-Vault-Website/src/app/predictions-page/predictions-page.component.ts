5cf5216814e62383bc940b851a80f3a1020dfa43
Dhashin Reddy <u18169253@tuks.co.za>
2022-09-27 09:49:22 +0200

    predictions age info

--- Frontend/Website/Vote-Vault-Website/src/app/predictions-page/predictions-page.component.ts
+++ Frontend/Website/Vote-Vault-Website/src/app/predictions-page/predictions-page.component.ts
@@ -63,3 +63,52 @@ maintainAspectRatio: false
 };
 
 }
+
+async showAgeInfo(){
+  this.div0=false;
+  this.div3=true;
+  this.div2=false;
+  this.div1=false
+
+  await this.dataService.fetchAllElections();
+  await this.dataService.calculateProbabilities();
+
+  // await this.dataService.fetchElections().then(()  =>  {
+  //   this.dataService.calculateProbabilities();
+  //  })
+
+  console.log(this.dataService.agesArray);
+
+
+  this.type2 = 'bar';
+  this.data2 = {
+    labels: ["18-28","29-38","39-48","49-58","59-68","69-78","79-88","89-98","99+"],
+    datasets: [
+      {
+        backgroundColor: ['#353535','#3c6e71','#b5fff8','#d9d9d9','#284b63','#4f6d7a','#c0d6df','#365535','#3d6e71'],
+        data: [this.dataService.agesArray[0] / (this.dataService.maleCount + this.dataService.femaleCount), this.dataService.agesArray[1] / (this.dataService.maleCount + this.dataService.femaleCount), this.dataService.agesArray[2] / (this.dataService.maleCount + this.dataService.femaleCount), this.dataService.agesArray[3] / (this.dataService.maleCount + this.dataService.femaleCount), this.dataService.agesArray[4] / (this.dataService.maleCount + this.dataService.femaleCount), this.dataService.agesArray[5] / (this.dataService.maleCount + this.dataService.femaleCount), this.dataService.agesArray[6] / (this.dataService.maleCount + this.dataService.femaleCount), this.dataService.agesArray[7] / (this.dataService.maleCount + this.dataService.femaleCount), this.dataService.agesArray[8] / (this.dataService.maleCount + this.dataService.femaleCount)]
+      }
+    ]
+  };
+  this.options2 = {
+    legend:{
+      display: false
+    },
+    title:{
+      display: true,
+      text:"Age Distribution Percentage"
+    },
+    scales : {
+      yAxes: [{
+         ticks: {
+            steps : 10,
+            stepValue : Math.max(10, this.dataService.agesArray[0], this.dataService.agesArray[1], this.dataService.agesArray[2], this.dataService.agesArray[3], this.dataService.agesArray[4], this.dataService.agesArray[5], this.dataService.agesArray[6], this.dataService.agesArray[7], this.dataService.agesArray[8]),
+            max : Math.max(this.dataService.agesArray[0], this.dataService.agesArray[1], this.dataService.agesArray[2], this.dataService.agesArray[3], this.dataService.agesArray[4], this.dataService.agesArray[5], this.dataService.agesArray[6], this.dataService.agesArray[7], this.dataService.agesArray[8]),
+            min: 0
+          }
+      }]
+    },
+    responsive: true,
+    maintainAspectRatio: false
+  };
+}
