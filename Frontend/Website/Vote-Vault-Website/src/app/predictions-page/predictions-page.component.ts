a178c90388b22eaa52f47a5dbe31ea89ff3c7482
Dhashin Reddy <u18169253@tuks.co.za>
2022-09-27 09:48:46 +0200

    predictions gender info

--- Frontend/Website/Vote-Vault-Website/src/app/predictions-page/predictions-page.component.ts
+++ Frontend/Website/Vote-Vault-Website/src/app/predictions-page/predictions-page.component.ts
@@ -19,3 +19,47 @@ div1:boolean=false;
 div2:boolean=false;
 div3:boolean=false;
 div4:boolean=false;
+
+async showGenderInfo(){
+  this.div0=false;
+  this.div1=true;
+  this.div2=false;
+  this.div3=false;
+
+  await this.dataService.fetchAllElections();
+  console.log("Shw gender: "+this.dataService.maleCount);
+  console.log("Shw gender: "+this.dataService.femaleCount);
+
+  this.type = 'bar';
+this.data = {
+labels: ["Male", "Female"],
+datasets: [
+  {
+    backgroundColor: ['#b56576','#e56b6f'],
+    data: [this.dataService.maleCount / (this.dataService.maleCount + this.dataService.femaleCount), this.dataService.femaleCount / (this.dataService.maleCount + this.dataService.femaleCount)]
+  }
+]
+};
+this.options = {
+legend:{
+  display: false
+},
+title:{
+  display: true,
+  text:"Gender Distribution Percentage"
+},
+scales : {
+  yAxes: [{
+     ticks: {
+        steps : 10,
+        stepValue : Math.max(10, this.dataService.maleCount, this.dataService.femaleCount),
+        max : Math.max(this.dataService.maleCount, this.dataService.femaleCount),
+        min: 0
+      }
+  }]
+},
+responsive: true,
+maintainAspectRatio: false
+};
+
+}
